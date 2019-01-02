c0812f839b51a5c7c2931627d8fc396136e61340
BradHacker <harkerbd@gmail.com>
2019-01-02 15:08:10 -0500

    Fix Module Imports

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
 const db = require('./db');
-const path = require('path');
-const userRouter = require(path.resolve('./Users'));
-const { accountsRouter, checkAdminExists } = require('./Accounts.js');
-const { sessionsRouter } = require('./Sessions.js');
-const { teamMembersRouter } = require('./TeamMembers.js');
-const { meetingsRouter } = require('./Meetings.js');
-const { googleRouter } = require('./Google.js');
+// console.log(`__dirname: ${__dirname}`);
+// console.log(`__dirname indexOf: ${__dirname.indexOf('/src/server')}`);
+const userRouter =
+  __dirname.indexOf('/src/server') !== -1 ? require('./Users') : require('/src/server/Users');
+const { accountsRouter, checkAdminExists } =
+  __dirname.indexOf('/src/server') !== -1 ? require('./Accounts') : require('/src/server/Accounts');
+const { sessionsRouter } =
+  __dirname.indexOf('/src/server') !== -1 ? require('./Sessions') : require('/src/server/Sessions');
+const { teamMembersRouter } =
+  __dirname.indexOf('/src/server') !== -1
+    ? require('./TeamMembers')
+    : require('/src/server/TeamMembers');
+const { meetingsRouter } =
+  __dirname.indexOf('/src/server') !== -1 ? require('./Meetings') : require('/src/server/Meetings');
+const { googleRouter } =
+  __dirname.indexOf('/src/server') !== -1 ? require('./Google') : require('/src/server/Google');
 
 // mongoose.set('debug', true);
 
